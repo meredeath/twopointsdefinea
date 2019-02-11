5250403a454c5718e26f2567030a12de67d43900
Meredith <mhu2@stuy.edu>
2019-02-11 00:23:22 -0500

    started on octants 1 & 2

--- draw.c
+++ draw.c
@@ -7,4 +7,46 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int x = x0;
+  int y = y0;
+  int a = (y1 - y0);
+  int b = -(x1 - x0);
+  int d = 2*a + b;
+
+  if(b != 0){
+    int m = a/-b;
+  }else{
+    int m = 0;
+  }
+  
+  //if it's in octant 1 or 5:
+  if(0 < m < 1){
+    while(x < x1){
+      plot(s,c,x,y);
+      if(d > 0){
+	y++;
+	d += 2*b;
+      }
+      x++;
+      d += 2*a;
+    }
+  }
+
+  //if it's in octant 2 or 6:
+  if(1 < m){
+    d = a + 2*b;
+    while(y < y1){
+      plot(s,c,x,y);
+      if(d <= 0){
+	x++;
+	d += 2*a;
+      }
+      y++;
+      d += 2*a;
+    }
+  }
+
+  //if it's in octant 3 or 7:
+
+  //if it's in octant 4 or 8:
 }
