3102f0ebc7e555174966b6b7b904f5e2fb4a3a6d
Meredith <mhu2@stuy.edu>
2019-02-11 01:30:52 -0500

    creativity is ... kind of hard to come by

--- main.c
+++ main.c
@@ -11,13 +11,19 @@ int main() {
   screen s;
   color c;
 
-  c.red = 0;
-  c.green = MAX_COLOR;
-  c.blue = 0;
-
   clear_screen(s);
+  
+  c.red = 0;
+  c.green = 0;
+  c.blue = MAX_COLOR;
 
+  int counter= 0;
+  for(int i = MAX_COLOR; i >= 0; i--){
+    c.blue = i;
+    draw_line(250,250,250,250+counter,s,c);
+    counter++;
+  }
 
-  display(s);
+  //display(s);
   save_extension(s, "lines.png");
 }
